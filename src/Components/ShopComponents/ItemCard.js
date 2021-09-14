077b4267cf30bc8c35a52e83473a04428a5c44a5
enPlace <n.place@protonmail.com>
2021-09-14 16:11:53 -0300

    Format shop prices with comma separators and 2 fraction digits

--- src/Components/ShopComponents/ItemCard.js
+++ src/Components/ShopComponents/ItemCard.js
@@ -3,18 +3,20 @@ import { useState } from "react";
 import shoppingCartIcon from "../../Assets/shopping-cart.svg";
 import { CircularProgress } from "@material-ui/core";
 import { useSelector, useDispatch } from "react-redux";
-import { selectCategoryBool, setNewCategorySelected } from "../../reducers/slices/newCategorySelectedSlice";
+import {
+  selectCategoryBool,
+  setNewCategorySelected,
+} from "../../reducers/slices/newCategorySelectedSlice";
 import { itemAdded } from "../../reducers/slices/cartSlice";
-
 
 const LoadPhoto = lazy(() => import("../LoadPhoto"));
 const loader = () => <CircularProgress></CircularProgress>;
 
 const ItemCard = ({ item }) => {
   const dispatch = useDispatch();
-  const categoryBool = useSelector(selectCategoryBool)
+  const categoryBool = useSelector(selectCategoryBool);
   const [count, setCount] = useState(0);
-  if (categoryBool && count!==0 ){
+  if (categoryBool && count !== 0) {
     setCount(0);
   }
   return (
@@ -38,7 +40,13 @@ const ItemCard = ({ item }) => {
         <div>{item.processor}</div>
         <div>{item.storage}</div>
         <div>{item.os}</div>
-        <div style={{ marginTottom: "auto" }}>${item.price}</div>
+        <div style={{ marginTottom: "auto" }}>
+          $
+          {parseFloat(item.price).toLocaleString("en-US", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+          })}
+        </div>
       </div>
       <div
         className="cartManager"
@@ -65,13 +73,15 @@ const ItemCard = ({ item }) => {
           action=""
           onSubmit={(e) => {
             e.preventDefault();
-          if(count>0) { dispatch(
-              itemAdded({
-                ...item,
-                count,
-              })
-            );
-            setCount(0);}
+            if (count > 0) {
+              dispatch(
+                itemAdded({
+                  ...item,
+                  count,
+                })
+              );
+              setCount(0);
+            }
           }}
         >
           {" "}
@@ -93,10 +103,13 @@ const ItemCard = ({ item }) => {
                 setCount(e.target.value);
               }}
             />
-            <button type="button" onClick={() => {
-              setCount(count + 1)
-              dispatch(setNewCategorySelected({bool:false}))
-              }}>
+            <button
+              type="button"
+              onClick={() => {
+                setCount(count + 1);
+                dispatch(setNewCategorySelected({ bool: false }));
+              }}
+            >
               +
             </button>
           </div>
